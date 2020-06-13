0da2a65acd8a9695f41b1465c23ab4a57dbec674
Noah Makau <noshmak@gmail.com>
2020-06-13 12:13:20 +0300

    correct errors on JS and html so far

--- js/scripts.js
+++ js/scripts.js
@@ -32,28 +32,28 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $("#work1").mouseover(function () {
+    $("#port1").mouseover(function () {
         $("#overlay1").show();
     }).mouseout(function () {
         $("#overlay1").hide();
     });
 });
 $(document).ready(function () {
-    $("#work2").mouseover(function () {
+    $("#port2").mouseover(function () {
         $("#overlay2").show();
     }).mouseout(function () {
         $("#overlay2").hide();
     });
 });
 $(document).ready(function () {
-    $("#work3").mouseover(function () {
+    $("#port3").mouseover(function () {
         $("#overlay3").show();
     }).mouseout(function () {
         $("#overlay3").hide();
     });
 });
 $(document).ready(function () {
-    $("#work4").mouseover(function () {
+    $("#port4").mouseover(function () {
         $("#overlay4").show();
     }).mouseout(function () {
         $("#overlay4").hide();
@@ -61,22 +61,22 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $("#work5").mouseover(function () {
+    $("#port5").mouseover(function () {
         $("#overlay5").show();
     }).mouseout(function () {
         $("#overlay5").hide();
     });
-    $("#work6").mouseover(function () {
+    $("#port6").mouseover(function () {
         $("#overlay6").show();
     }).mouseout(function () {
         $("#overlay6").hide();
     });
-    $("#work7").mouseover(function () {
+    $("#port7").mouseover(function () {
         $("#overlay7").show();
     }).mouseout(function () {
         $("#overlay7").hide();
     });
-    $("#work8").mouseover(function () {
+    $("#port8").mouseover(function () {
         $("#overlay8").show();
     }).mouseout(function () {
         $("#overlay8").hide();
